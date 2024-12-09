9aa46e2e3cbb1b5c6113ef2b89ce94436e66f9ad
ChristopheKunzli <christophe.kunzliv@heig-vd.ch>
2024-12-09 19:23:27 +0100

    Day 6 with multithreading

--- src/Day6.java
+++ src/Day6.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Day6 {
     static class Point {
@@ -45,8 +47,8 @@ public class Day6 {
         }
     }
 
-    private char getNextDirection() {
-        return switch (currentPos.direction) {
+    private char getNextDirection(Point p) {
+        return switch (p.direction) {
             case '^' -> '>';
             case '>' -> 'v';
             case 'v' -> '<';
@@ -71,7 +73,7 @@ public class Day6 {
 
             char next = grid[nextX][nextY];
             if (next == '#') {
-                currentPos.direction = getNextDirection();
+                currentPos.direction = getNextDirection(currentPos);
             } else {
                 currentPos.x = nextX;
                 currentPos.y = nextY;
@@ -90,63 +92,83 @@ public class Day6 {
         return count;
     }
 
-    private boolean isInBounds(int x, int y) {
-        return x >= 0 && y >= 0 && x < grid.length && y < grid[x].length;
-    }
+    class Job implements Runnable {
+        int x, y;
+        private char[][] jobGrid;
+        private final Point jobCurrentPos;
+        boolean hasLoop = false;
 
-    private boolean hasLoop(char[][] gridCopy) {
-        Set<String> visited = new HashSet<>();
+        public Job(int x, int y, Point initialPos) {
+            this.x = x;
+            this.y = y;
+            jobCurrentPos = initialPos;
+        }
 
-        while (true) {
-            if (visited.contains(currentPos.toString())) {
-                return true;
+        private boolean isInBounds(int x, int y) {
+            return x >= 0 && y >= 0 && x < jobGrid.length && y < jobGrid[x].length;
+        }
+
+        private boolean hasLoop() {
+            Set<String> visited = new HashSet<>();
+
+            while (true) {
+                if (visited.contains(jobCurrentPos.toString())) {
+                    return true;
+                }
+
+                visited.add(jobCurrentPos.toString());
+                int nextX = jobCurrentPos.x, nextY = jobCurrentPos.y;
+                switch (jobCurrentPos.direction) {
+                    case '^' -> --nextX;
+                    case 'v' -> ++nextX;
+                    case '<' -> --nextY;
+                    case '>' -> ++nextY;
+                }
+
+                if (!isInBounds(nextX, nextY)) {
+                    return false;
+                }
+
+                char next = jobGrid[nextX][nextY];
+                if (next == '#') {
+                    jobCurrentPos.direction = getNextDirection(jobCurrentPos);
+                } else {
+                    jobCurrentPos.x = nextX;
+                    jobCurrentPos.y = nextY;
+                    jobGrid[jobCurrentPos.x][jobCurrentPos.y] = 'X';
+                }
+
             }
+        }
 
-            visited.add(currentPos.toString());
-            int nextX = currentPos.x, nextY = currentPos.y;
-            switch (currentPos.direction) {
-                case '^' -> --nextX;
-                case 'v' -> ++nextX;
-                case '<' -> --nextY;
-                case '>' -> ++nextY;
+        @Override
+        public void run() {
+            jobGrid = new char[initialGrid.length][];
+            for (int i = 0; i < initialGrid.length; ++i) {
+                jobGrid[i] = initialGrid[i].clone();
             }
-
-            if (!isInBounds(nextX, nextY)) {
-                return false;
-            }
-
-            char next = gridCopy[nextX][nextY];
-            if (next == '#') {
-                currentPos.direction = getNextDirection();
-            } else {
-                currentPos.x = nextX;
-                currentPos.y = nextY;
-                gridCopy[currentPos.x][currentPos.y] = 'X';
-            }
-
+            jobGrid[x][y] = '#';
+            jobGrid[initialPos.x][initialPos.y] = 'X';
+            hasLoop = hasLoop();
         }
     }
 
     public int part2() {
-        int count = 0;
+        List<Job> jobs = new ArrayList<>();
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; ++j) {
                 if (grid[i][j] == 'X') {
-                    currentPos.x = initialPos.x;
-                    currentPos.y = initialPos.y;
-                    currentPos.direction = initialPos.direction;
-                    char[][] gridCopy = new char[grid.length][grid[i].length];
-                    for (int k = 0; k < gridCopy.length; ++k) {
-                        gridCopy[k] = initialGrid[k].clone();
-                    }
-                    gridCopy[i][j] = '#';
-                    gridCopy[initialPos.x][initialPos.y] = 'X';
-                    if (hasLoop(gridCopy)) {
-                        ++count;
-                    }
+                    jobs.add(new Job(i, j, new Point(initialPos.x, initialPos.y, initialPos.direction)));
+                    executor.execute(jobs.getLast());
                 }
             }
         }
-        return count;
+        executor.close();
+        int sum = 0;
+        for (Job job : jobs) {
+            if (job.hasLoop) ++sum;
+        }
+        return sum;
     }
 }
