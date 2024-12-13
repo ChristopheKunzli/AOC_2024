5e7be866429be79a2a3711110b1165bed2961365
ChristopheKunzli <christophe.kunzliv@heig-vd.ch>
2024-12-13 11:46:05 +0100

    Simplify structure and better name

--- src/Day13.java
+++ src/Day13.java
@@ -11,33 +11,18 @@ public class Day13 {
         System.out.println("Part 2: " + part2());
     }
 
-    static class Equation {
-        long x, y;
-
-        public Equation(long x, long y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public String toString() {
-            return "x=" + x + ", y=" + y;
-        }
-    }
+    record ButtonMovement(long x, long y) { }
 
     static class Machine {
-        Equation equation1;
-        Equation equation2;
+        ButtonMovement buttonMovement1;
+        ButtonMovement buttonMovement2;
         long prizeX, prizeY;
 
-        public Machine(Equation equation1, Equation equation2, int prizeX, int prizeY) {
-            this.equation1 = equation1;
-            this.equation2 = equation2;
+        public Machine(ButtonMovement buttonMovement1, ButtonMovement buttonMovement2, int prizeX, int prizeY) {
+            this.buttonMovement1 = buttonMovement1;
+            this.buttonMovement2 = buttonMovement2;
             this.prizeX = prizeX;
             this.prizeY = prizeY;
-        }
-
-        public String toString() {
-            return "Equation 1: " + equation1 + ", Equation 2: " + equation2 + ", Prize: x=" + prizeX + ", y=" + prizeY;
         }
     }
 
@@ -47,20 +32,20 @@ public class Day13 {
             String[] parts1 = lines.get(i).replace("Button A: X+", "").split(", Y+");
             String[] parts2 = lines.get(i + 1).replace("Button B: X+", "").split(", Y+");
             String[] parts3 = lines.get(i + 2).replace("Prize: X=", "").split(", Y=");
-            Equation equation1 = new Equation(Integer.parseInt(parts1[0]), Integer.parseInt(parts1[1]));
-            Equation equation2 = new Equation(Integer.parseInt(parts2[0]), Integer.parseInt(parts2[1]));
-            machines.add(new Machine(equation1, equation2, Integer.parseInt(parts3[0]), Integer.parseInt(parts3[1])));
+            ButtonMovement buttonMovement1 = new ButtonMovement(Integer.parseInt(parts1[0]), Integer.parseInt(parts1[1]));
+            ButtonMovement buttonMovement2 = new ButtonMovement(Integer.parseInt(parts2[0]), Integer.parseInt(parts2[1]));
+            machines.add(new Machine(buttonMovement1, buttonMovement2, Integer.parseInt(parts3[0]), Integer.parseInt(parts3[1])));
             i += 4;
         }
         return machines;
     }
 
     private long compute(Machine machine) {
-        long b_term1 = machine.prizeY * machine.equation1.x - machine.prizeX * machine.equation1.y;
-        long b_term2 = machine.equation2.y * machine.equation1.x - machine.equation2.x * machine.equation1.y;
+        long b_term1 = machine.prizeY * machine.buttonMovement1.x - machine.prizeX * machine.buttonMovement1.y;
+        long b_term2 = machine.buttonMovement2.y * machine.buttonMovement1.x - machine.buttonMovement2.x * machine.buttonMovement1.y;
 
-        long a_term1 = machine.prizeX - (machine.equation2.x * (b_term1 / b_term2));
-        long a_term2 = machine.equation1.x;
+        long a_term1 = machine.prizeX - (machine.buttonMovement2.x * (b_term1 / b_term2));
+        long a_term2 = machine.buttonMovement1.x;
 
         if (b_term1 % b_term2 != 0 || a_term1 % a_term2 != 0) {
             return 0L;
