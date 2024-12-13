23bdca61676161b380f08b20d210503e8080d83f
ChristopheKunzli <christophe.kunzliv@heig-vd.ch>
2024-12-13 11:49:55 +0100

    Make name more explicit

--- src/Day13.java
+++ src/Day13.java
@@ -11,16 +11,16 @@ public class Day13 {
         System.out.println("Part 2: " + part2());
     }
 
-    record ButtonMovement(long x, long y) { }
+    record Button(long x, long y) { }
 
     static class Machine {
-        ButtonMovement buttonMovement1;
-        ButtonMovement buttonMovement2;
+        Button a;
+        Button b;
         long prizeX, prizeY;
 
-        public Machine(ButtonMovement buttonMovement1, ButtonMovement buttonMovement2, int prizeX, int prizeY) {
-            this.buttonMovement1 = buttonMovement1;
-            this.buttonMovement2 = buttonMovement2;
+        public Machine(Button a, Button b, int prizeX, int prizeY) {
+            this.a = a;
+            this.b = b;
             this.prizeX = prizeX;
             this.prizeY = prizeY;
         }
@@ -32,29 +32,29 @@ public class Day13 {
             String[] parts1 = lines.get(i).replace("Button A: X+", "").split(", Y+");
             String[] parts2 = lines.get(i + 1).replace("Button B: X+", "").split(", Y+");
             String[] parts3 = lines.get(i + 2).replace("Prize: X=", "").split(", Y=");
-            ButtonMovement buttonMovement1 = new ButtonMovement(Integer.parseInt(parts1[0]), Integer.parseInt(parts1[1]));
-            ButtonMovement buttonMovement2 = new ButtonMovement(Integer.parseInt(parts2[0]), Integer.parseInt(parts2[1]));
-            machines.add(new Machine(buttonMovement1, buttonMovement2, Integer.parseInt(parts3[0]), Integer.parseInt(parts3[1])));
+            Button button1 = new Button(Integer.parseInt(parts1[0]), Integer.parseInt(parts1[1]));
+            Button button2 = new Button(Integer.parseInt(parts2[0]), Integer.parseInt(parts2[1]));
+            machines.add(new Machine(button1, button2, Integer.parseInt(parts3[0]), Integer.parseInt(parts3[1])));
             i += 4;
         }
         return machines;
     }
 
     private long compute(Machine machine) {
-        long b_term1 = machine.prizeY * machine.buttonMovement1.x - machine.prizeX * machine.buttonMovement1.y;
-        long b_term2 = machine.buttonMovement2.y * machine.buttonMovement1.x - machine.buttonMovement2.x * machine.buttonMovement1.y;
+        long b_term1 = machine.prizeY * machine.a.x - machine.prizeX * machine.a.y;
+        long b_term2 = machine.b.y * machine.a.x - machine.b.x * machine.a.y;
 
-        long a_term1 = machine.prizeX - (machine.buttonMovement2.x * (b_term1 / b_term2));
-        long a_term2 = machine.buttonMovement1.x;
+        long a_term1 = machine.prizeX - (machine.b.x * (b_term1 / b_term2));
+        long a_term2 = machine.a.x;
 
         if (b_term1 % b_term2 != 0 || a_term1 % a_term2 != 0) {
             return 0L;
         }
 
-        long b = b_term1 / b_term2;
-        long a = a_term1 / a_term2;
+        long bPressCount = b_term1 / b_term2;
+        long aPressCount = a_term1 / a_term2;
 
-        return a * 3L + b;
+        return aPressCount * 3L + bPressCount;
     }
 
     private long part1() {
