ac2bebde8ded253e265e593f9b3f223da2e039e9
ChristopheKunzli <christophe.kunzliv@heig-vd.ch>
2024-12-18 13:34:47 +0100

    Day 18 part 2

--- src/Day18.java
+++ src/Day18.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
 public class Day18 {
-    private final static int DIMENSION = 7;
-    private final static int BYTES = 12;
+    private final static int DIMENSION = 71;
+    private final static int BYTES = 1024;
 
     record Coord(int x, int y) {
         public String toString() {
-            return x + ";" + y;
+            return x + "," + y;
         }
     }
 
@@ -16,7 +16,7 @@ public class Day18 {
         parse(Utils.getStrings(filePath));
         System.out.println("Day 18");
         System.out.println("Part 1: " + part1());
-        System.out.println("Part 2: ");
+        System.out.println("Part 2: " + part2());
     }
 
     private void parse(List<String> lines) {
@@ -30,20 +30,7 @@ public class Day18 {
         return c.x >= 0 && c.y >= 0 && c.x < DIMENSION && c.y < DIMENSION;
     }
 
-    private int part1() {
-        char[][] grid = new char[DIMENSION][DIMENSION];
-        for (int i = 0; i < BYTES; ++i) {
-            grid[coords.get(i).y][coords.get(i).x] = '#';
-        }
-        for (int i = 0; i < grid.length; ++i) {
-            for (int j = 0; j < grid[i].length; ++j) {
-                if (grid[i][j] != '#') grid[i][j] = '.';
-                System.out.print(grid[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-
+    private int bfs(char[][] grid) {
         Queue<Coord> queue = new LinkedList<>();
         queue.add(new Coord(0, 0));
         Set<String> seen = new HashSet<>();
@@ -63,10 +50,13 @@ public class Day18 {
                 Coord up = new Coord(current.x, current.y - 1);
                 Coord down = new Coord(current.x, current.y + 1);
 
-                if (isInBounds(left) && !seen.contains(left.toString()) && grid[left.y][left.x] == '.') nextStep.add(left);
-                if (isInBounds(right) && !seen.contains(right.toString()) && grid[right.y][right.x] == '.') nextStep.add(right);
+                if (isInBounds(left) && !seen.contains(left.toString()) && grid[left.y][left.x] == '.')
+                    nextStep.add(left);
+                if (isInBounds(right) && !seen.contains(right.toString()) && grid[right.y][right.x] == '.')
+                    nextStep.add(right);
                 if (isInBounds(up) && !seen.contains(up.toString()) && grid[up.y][up.x] == '.') nextStep.add(up);
-                if (isInBounds(down) && !seen.contains(down.toString()) && grid[down.y][down.x] == '.') nextStep.add(down);
+                if (isInBounds(down) && !seen.contains(down.toString()) && grid[down.y][down.x] == '.')
+                    nextStep.add(down);
 
                 seen.add(left.toString());
                 seen.add(right.toString());
@@ -77,6 +67,33 @@ public class Day18 {
             ++pathLength;
         }
 
-        return pathLength;
+        return -1;//if no path found
+    }
+
+    private char[][] initGrid(int height, int width, int steps) {
+        char[][] grid = new char[height][width];
+        for (int i = 0; i < steps; ++i) {
+            grid[coords.get(i).y][coords.get(i).x] = '#';
+        }
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[i].length; ++j) {
+                if (grid[i][j] != '#') grid[i][j] = '.';
+            }
+        }
+        return grid;
+    }
+
+    private int part1() {
+        return bfs(initGrid(DIMENSION, DIMENSION, BYTES));
+    }
+
+    private String part2() {
+        char[][] grid = initGrid(DIMENSION, DIMENSION, BYTES);
+        int count = BYTES;
+        while (bfs(grid) != -1) {
+            grid[coords.get(count).y][coords.get(count).x] = '#';
+            ++count;
+        }
+        return coords.get(count - 1).toString();
     }
 }
